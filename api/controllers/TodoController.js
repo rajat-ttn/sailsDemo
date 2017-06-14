78b09734ed97621fa9c2d34c42871ab0ce677e17
Rajat Sharma <rajat.sharma@tothenew.com>
2017-06-14 22:58:28 +0530

    no message

--- api/controllers/TodoController.js
+++ api/controllers/TodoController.js
@@ -35,25 +35,3 @@ module.exports = {
     })
   }
 };
-
-
-
-function a(req,res) {
-
-  var data_from_client = req.params.all();
-
-  if(req.isSocket && req.method === 'POST'){
-
-// This is the message from connected client
-// So add new conversation
-    Chat.create(data_from_client)
-      .exec(function(error,data_from_client){
-        console.log(data_from_client);
-        Chat.publishCreate({id: data_from_client.id, message : data_from_client.message , user:data_from_client.user});
-      });
-  }
-  else if(req.isSocket){
-    Chat.watch(req.socket);
-    console.log( 'User subscribed to ' + req.socket.id );
-  }
-}
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -17,7 +17,8 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+  'styles/**/*.css',
+  'bower_components/bootstrap/dist/css/bootstrap.css'
 ];
 
 
